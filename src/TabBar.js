9d78a6e539779f7b05dc9b6f7f02fc2a7c98fe1f
Lera <58082566+leralera2201@users.noreply.github.com>
2021-03-29 15:52:40 +0300

    remove deprecated method

--- src/TabBar.js
+++ src/TabBar.js
@@ -409,7 +409,7 @@ export default class TabBar<T: *> extends React.Component<Props<T>, State> {
             onMomentumScrollBegin={this._handleMomentumScrollBegin}
             onMomentumScrollEnd={this._handleMomentumScrollEnd}
             contentOffset={this.state.initialOffset}
-            ref={el => (this._scrollView = el && el.getNode())}
+            ref={el => (this._scrollView = el)}
           >
             {routes.map((route, i) => {
               const outputRange = inputRange.map(
